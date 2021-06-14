a23a7ff268490f67fdb789da5d3f8b1d95f90cd2
Chaitanya <892599@cognizant.com>
2021-06-14 14:27:33 +0530

    second commit

---
+++ Assessment 1_1/BlankAndWhite.java
@@ -0,0 +1,5 @@
+public class BlankAndWhite extends TV{
+    public void print(){
+        System.out.println("You have selected Black and White TV ");
+    }
+}
---
+++ Assessment 1_1/Color.java
@@ -0,0 +1,6 @@
+public class Color extends TV{
+    @Override
+    public void print() {
+        System.out.println("You have selected Color TV");
+    }
+}
