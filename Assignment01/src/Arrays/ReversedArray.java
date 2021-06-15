2dfa1a58265bfdbffa6320be0024207b0f2bb467
NGeethaReddy <85727862+NGeethaReddy@users.noreply.github.com>
2021-06-15 12:58:30 +0530

    Update ReversedArray.java
    space added below main method at line 9
    space added below int vowelsCount at line 16
    space added below reversedString.append(letter) at line 21

--- Assignment01/src/Arrays/ReversedArray.java
+++ Assignment01/src/Arrays/ReversedArray.java
@@ -6,18 +6,23 @@ public class ReversedArray {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        
         StringBuffer reversedString = new StringBuffer();
         String sample = "Hello Team";
         System.out.println("Enter a String : ");
         sample = sc.nextLine();
         char[] vowels = {'a', 'e', 'i', 'o', 'u'};
         int vowelsCount = 0;
+        
         for (int i = sample.length()-1; i >= 0; i--) {
+            
             Character letter = sample.charAt(i);
             reversedString.append(letter);
+            
             for (char c : vowels) {
                 if ((c == Character.toLowerCase(letter))) {
                     vowelsCount++;
+                    
                 }
             }
         }
