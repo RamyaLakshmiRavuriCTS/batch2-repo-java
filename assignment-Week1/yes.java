eba1259db7ad5b48c08958d6790f41f69afa41c0
Yeshwanth Kumar <yeshwanthkumar40@gmail.com>
2021-06-15 18:09:55 +0530

    Yeshwanth assignment-Week1

--- assignment-Week1/yes.java
+++ assignment-Week1/yes.java
@@ -1,17 +1,19 @@
 import java.util.Scanner;
 
 public class yes {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Choose your selection:" +
                 "\n1. Black and White TV" +
                 "\n2. Color TV");
         int select = sc.nextInt();
+
         if (select == 1) {
             BlackandWhite tv = new BlackandWhite();
             tv.selectionofTv();
         } else if (select == 2) {
-            BlackandWhite tv = new Color();
+            Color tv = new Color();
             tv.selectionofTv();
         } else {
             System.out.println("Invalid");
@@ -19,7 +21,8 @@ public class yes {
     }
 }
 
-class BlackandWhite extends TV {
+ class BlackandWhite extends TV {
+
     @Override
     public void selectionofTv() {
 
@@ -27,15 +30,19 @@ class BlackandWhite extends TV {
     }
 }
 
-class Color extends TV {
+ class Color extends TV {
+
     @Override
     public void selectionofTv() {
 
         System.out.println("You have selected Color TV");
     }
 }
-class TV {
+
+ class TV {
+
     public void selectionofTv() {
+
         System.out.println("You have selected a TV");
     }
  }
