652fdaa1407b37d53fc4ab3f121382c7b66d0ca1
Rayudu <890828@cognizant.com>
2021-06-17 02:59:21 -0700

    Assignment-1

--- assignment/Dynamic Binding/TelevisionSelection.java
+++ assignment/Dynamic Binding/TelevisionSelection.java
@@ -1,22 +1,22 @@
 import java.util.Scanner;
+
 public class TelevisionSelection {
     public static void main(String [] args){
-        TelevisionSelection tel = new TelevisionSelection();
         Scanner scanner=new Scanner(System.in);
         System.out.println("Enter your Selection Number:\n 1 - Black and White TV \n 2 - Color TV");
         int selection=scanner.nextInt();
-        tel.chooseTypeOfTv(selection);
-    }
-    public void chooseTypeOfTv(int selection){
         switch(selection){
             case 1:
-                System.out.println("You have selected Black and White Tv");
+                Tv tv = new BlackAndWhiteTv();
+                tv.chooseTypeOfTv();
                 break;
             case 2:
-                System.out.println("You have selected Color Tv");
+                Tv tv1 = new ColorTv();
+                tv1.chooseTypeOfTv();
                 break;
             default :
                 System.out.println("Invalid Selection");
         }
     }
+
 }
