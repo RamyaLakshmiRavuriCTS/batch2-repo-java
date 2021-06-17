bcd8629c453611f354ed9b7ee566d09d76b2de8c
C <890773@cognizant.com>
2021-06-17 10:32:44 +0530

    Week2 assignment

---
+++ assignment/src/com/assignment/Week2/Interface example/Honda.java
@@ -0,0 +1,7 @@
+public class Honda implements Bike {
+    @Override
+    public void Company()
+    {
+        System.out.println("Bike is Honda");
+    }
+}
---
+++ assignment/src/com/assignment/Week2/Interface example/RoyalEnfield.java
@@ -0,0 +1,6 @@
+public class RoyalEnfield implements Bike {
+    @Override
+    public void Company() {
+        System.out.println("Bike is Royal Enfeild");
+    }
+}
