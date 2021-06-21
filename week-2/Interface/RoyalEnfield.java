08f79f9821c80f6bb1ae2d75ec36a36923070dfe
Rayudu <890828@cognizant.com>
2021-06-20 23:06:16 -0700

    Modified

---
+++ week-2/Interface/RoyalEnfield.java
@@ -0,0 +1,17 @@
+public class RoyalEnfield implements Bike{
+    int gear;
+    String name;
+    @Override
+    public void changeGear(int newGear) {
+        gear= newGear;
+        System.out.println("Speed: "+getSpeedLimit()+" Gear: "+gear);
+    }
+
+    @Override
+    public void move(String name) {
+        System.out.println(name+" is moving with "+getSpeedLimit()+"KMPH speed");
+    }
+
+
+
+}
