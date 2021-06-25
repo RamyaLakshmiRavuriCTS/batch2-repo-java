d2b307f1801b7748a1243a4c87a56bbe1259e211
KalpanaKunchapu <85940868+KalpanaKunchapu@users.noreply.github.com>
2021-06-25 12:35:48 +0530

    Update AvoidDeadLock.java

--- assignment/assignment3/Threading/AvoidDeadLock.java
+++ assignment/assignment3/Threading/AvoidDeadLock.java
@@ -6,6 +6,7 @@ public class AvoidDeadLock {
         Object object3 = new Object();
         java.lang.Thread thread1 = new java.lang.Thread(new ThreadSynchronizedExample(object1, object2), "thread1");
         java.lang.Thread thread2 = new java.lang.Thread(new ThreadSynchronizedExample(object2, object3), "thread2");
+        
         thread1.start();
         try {
             java.lang.Thread.sleep(2000);
@@ -13,6 +14,7 @@ public class AvoidDeadLock {
             e.printStackTrace();
         }
         thread2.start();
+        
         try {
             java.lang.Thread.sleep(2000);
         } catch (InterruptedException e) {
