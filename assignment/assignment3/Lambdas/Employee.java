323b0cadd2ace5583ffd9297646d5cf384d7e617
KalpanaKunchapu <85940868+KalpanaKunchapu@users.noreply.github.com>
2021-06-25 12:28:17 +0530

    Update Employee.java

--- assignment/assignment3/Lambdas/Employee.java
+++ assignment/assignment3/Lambdas/Employee.java
@@ -3,10 +3,14 @@ import java.util.Comparator;
 public class Employee implements Comparable, Comparator {
     final private String department;
     final private int salary;
+    
+    
     public Employee(String department, int salary) {
         this.department = department;
         this.salary = salary;
     }
+    
+    
     @Override
     public String toString() {
         return "Employee{" +
@@ -14,24 +18,35 @@ public class Employee implements Comparable, Comparator {
                 ", salary=" + salary +
                 '}';
     }
+    
+    
     public String getDepartment() {
         return department;
     }
+    
     public int getSalary() {
         return salary;
     }
+    
+    
     @Override
     public int hashCode() {
         return this.getDepartment().hashCode();
     }
+    
+    
     @Override
     public int compare(Object o1, Object o2) {
         return Integer.compare(((Employee) o1).getSalary(), ((Employee) o2).getSalary());
     }
+    
+    
     @Override
     public boolean equals(Object obj) {
         return ((this.getDepartment().equals(((Employee) obj).getDepartment())));
     }
+    
+    
     @Override
     public int compareTo(Object o) {
         return Integer.compare(this.getSalary(), ((Employee) o).getSalary());
