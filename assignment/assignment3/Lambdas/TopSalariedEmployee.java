f6fa90fe5931337aec0f2801b93a56ccf86fa082
Kunchapu <890754@cognizant.com>
2021-06-25 18:19:48 +0530

    Updated Assignment3

---
+++ assignment/assignment3/Lambdas/TopSalariedEmployee.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class TopSalariedEmployee {
+    private final List<Employee> list = new ArrayList<>();
+    {
+        list.add(new Employee("CSE", 50000));
+        list.add(new Employee("IT", 20000));
+        list.add(new Employee("EEE", 40000));
+        list.add(new Employee("CSE", 30000));
+        list.add(new Employee("IT", 60000));
+    }
+    public static void main(String[] args) {
+        TopSalariedEmployee top = new TopSalariedEmployee();
+        top.list.stream().sorted(Comparator.reverseOrder()).distinct().forEach(System.out::println);
+    }
+}
---
+++ assignment/assignment3/Threading/JavaThreads.java
@@ -0,0 +1,33 @@
+public class JavaThreads implements Runnable {
+    @Override
+    public void run() {
+        Lock();
+    }
+
+    void Lock() {
+        System.out.println(Thread.currentThread().getName());
+        synchronized (JavaThreads.class) {
+            System.out.println("In block " + Thread.currentThread().getName());
+            System.out.println("In block " + Thread.currentThread().getName() + " end");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        JavaThreads runn = new JavaThreads();
+        Thread thread1 = new Thread(runn);
+        thread1.setName("Thread1 ");
+        thread1.start();
+
+        JavaThreads run2 = new JavaThreads();
+        Thread thread2 = new Thread(run2);
+        thread2.setName("Thread2");
+        thread2.start();
+
+        JavaThreads run3 = new JavaThreads();
+        Thread thread3 = new Thread(run3);
+        thread3.setName("Thread3");
+        thread3.start();
+
+    }
+}
