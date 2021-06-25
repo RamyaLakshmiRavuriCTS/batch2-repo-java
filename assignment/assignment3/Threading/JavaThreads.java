3c63eb66cdf1353f9ef0b1fc6202055948534f77
KalpanaKunchapu <85940868+KalpanaKunchapu@users.noreply.github.com>
2021-06-25 12:36:53 +0530

    Update JavaThreads.java

--- assignment/assignment3/Threading/JavaThreads.java
+++ assignment/assignment3/Threading/JavaThreads.java
@@ -18,10 +18,12 @@ public class JavaThreads implements Runnable {
         Thread thread1 = new Thread(runn);
         thread1.setName("Thread1 ");
         thread1.start();
+        
         JavaThreads run2 = new JavaThreads();
         Thread thread2 = new Thread(run2);
         thread2.setName("Thread2");
         thread2.start();
+        
         JavaThreads run3 = new JavaThreads();
         Thread thread3 = new Thread(run3);
         thread3.setName("Thread3");
