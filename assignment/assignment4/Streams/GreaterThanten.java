fe536ac85801a786d7700b6fef12ffa13804d3de
KalpanaKunchapu <85940868+KalpanaKunchapu@users.noreply.github.com>
2021-06-28 19:42:58 +0530

    Update GreaterThanten.java

--- assignment/assignment4/Streams/GreaterThanten.java
+++ assignment/assignment4/Streams/GreaterThanten.java
@@ -7,5 +7,6 @@ public class GreaterThanten {
     public static void main(String[] args) {
         List<Integer> number = Arrays.asList(1, 13, 4, 5, 29, 16, 19, 22, 6, 9, 35);
         number.stream().filter(integer -> integer > 10).forEach(System.out::println);
+        
     }
 }
