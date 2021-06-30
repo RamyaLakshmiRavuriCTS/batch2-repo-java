cb0ce0bbcba7dbb39c55b4f607016cfcae675092
GangadharYalasa <85686732+GangadharYalasa@users.noreply.github.com>
2021-06-30 13:02:02 +0530

    Update Main.java

--- assignment/src/Week3/Exceptions/NullPointException/src/Main.java
+++ assignment/src/Week3/Exceptions/NullPointException/src/Main.java
@@ -1,25 +1,21 @@
 import java.util.Scanner;
-
 //main class
-public class Main {
+public class MultipleException {
     //main method
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int a = sc.nextInt();
         int b = sc.nextInt();
         sc.nextLine();
-
+         String s= sc.nextLine();
         try {
-            String s = null;
             int c = a / b;
             System.out.println(s.length());
-        } catch (NullPointerException e) {
+        } catch (ArithmeticException | NullPointerException e) {
 
             System.out.println("End of main due to null input");
-        } catch (Exception e1) {
-
-            System.out.println("End of main due to divide by zero");
         }
+
     }
 
 }
