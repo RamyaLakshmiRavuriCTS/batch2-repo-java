fbece544ad7d0507dceaab8e5d68283385619a9c
Kunchapu <890754@cognizant.com>
2021-07-12 12:32:54 +0530

    Question2

---
+++ assignment/CodingExercises/Question2/UniqueAndEvenSum.java
@@ -0,0 +1,37 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Scanner;
+
+public class UniqueAndEvenSum {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter Size of array:");
+		int size = scanner.nextInt();
+
+		System.out.println("Enter Input Array:");
+		int array[] = new int[size];
+		
+		for (int i = 0; i < size; i++) {
+			array[i] = scanner.nextInt();
+		}
+
+		int sum = 0;
+		LinkedHashSet<Integer> val = new LinkedHashSet<Integer>();
+		
+		for (int i = 0; i < array.length; i++) {
+			val.add(array[i]);
+		}
+
+		Iterator<Integer> even = val.iterator();
+		while (even.hasNext()) {
+			int number2 = even.next();
+			if (number2 % 2 == 0)
+				sum = sum + number2;
+		}
+
+		if (sum == 0)
+			System.out.println("No even numbers");
+
+		System.out.println("sum" + sum);
+	}
+}
