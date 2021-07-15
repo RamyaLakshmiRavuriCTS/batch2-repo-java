75478641ccefecdcbcb13524a2f6ce460873797f
Naresh <naresh9badisagmail.com>
2021-07-15 11:20:46 +0530

    Naresh Coding Exercise question 6 and 7

---
+++ assignment/src/com/codingExercises/question6/Main.java
@@ -0,0 +1,20 @@
+package question6;
+
+import java.util.Scanner;
+
+//main class
+public class Main {
+
+    //main method
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        String sentence1=scanner.nextLine();
+        String sentence2=scanner.nextLine();
+        int result=UserMainCode.getAnagram(sentence1,sentence2);
+        if(result==1){
+            System.out.println("Anagrams");
+        }else{
+            System.out.println("Not Anagrams");
+        }
+    }
+}
