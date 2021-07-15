77b1ca8ee59911d93dae05f78239093ec342c1fe
892599 <85674034+892599@users.noreply.github.com>
2021-07-15 11:07:29 +0530

    Feature/krishna java core (#79)
    * krishna- coreJava
    
    * krishna javacore
    
    * krishna- coreJava
    
    * krishna- javaCore
    
    Co-authored-by: Chaitanya <892599@cognizant.com>

---
+++ assignment/src/com/CoreJavaProblems/Question2/UniqueEvenSum.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class UniqueEvenSum {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the size of array");
+        int size = input.nextInt();
+        int[] array = new int[size];
+        System.out.println("Enter the array elements");
+        for (int i = 0; i < size; i++) {
+            array[i] = input.nextInt();
+        }
+        int result = addUniqueEven(array);
+        if (result == -1)
+            System.out.println("NO EVEN NUMBER");
+        else
+            System.out.println(result);
+    }
+
+    public static int addUniqueEven(int[] array) {
+        HashSet<Integer> set=new HashSet<Integer>();
+        int sum = 0;
+        for (int i = 0; i < array.length; i++) {
+            if(set.contains(array[i]));
+            else {
+                if (array[i] % 2 == 0)
+                    sum += array[i];
+                set.add(array[i]);
+            }
+        }
+        if (sum != 0)
+            return sum;
+        return -1;
+    }
+}
---
+++ assignment/src/com/CoreJavaProblems/Question3/StringOccurrences.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class StringOccurrences {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the first sentence");
+        String words1 = input.nextLine();
+        System.out.println("Enter the second sentence");
+        String words2 = input.nextLine();
+        int result = countNoOfWords(words1, words2);
+        System.out.println("The no of occurences of word in first sentence : " + result);
+    }
+
+    public static int countNoOfWords(String arg1, String arg2) {
+        String[] input1 = arg1.split(" ");
+        String[] input2 = arg2.split(" ");
+        int count = 0;
+        for (int i = 0; i < input1.length; i++) {
+            if (input1[i].equals(input2[1])) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
