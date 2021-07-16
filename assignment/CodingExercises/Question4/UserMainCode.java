0312dae14b36a2458a1810c4286dcd6e19042621
KalpanaKunchapu <85940868+KalpanaKunchapu@users.noreply.github.com>
2021-07-16 12:08:31 +0530

    Update UserMainCode.java

--- assignment/CodingExercises/Question4/UserMainCode.java
+++ assignment/CodingExercises/Question4/UserMainCode.java
@@ -7,7 +7,7 @@ public class UserMainCode {
 	
 	public static List<Integer> generateOddEvenList(List<Integer> l1,List<Integer>l2){
 		
-		List<Integer> finalList =new ArrayList<Integer>();
+		List<Integer> finalList = new ArrayList<Integer>();
 				
 		for (int i=0; i<l1.size();i++) {
 			if(i%2!=0)
