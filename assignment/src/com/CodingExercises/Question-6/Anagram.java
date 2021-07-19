e80e0b1a4000e96d993e43d29e020be4a2c465a2
Yeshwanth Kumar <yeshwanthkumar40@gmail.com>
2021-07-19 13:08:34 +0530

    Yeshwanth modified Q6

--- assignment/src/com/CodingExercises/Question-6/Anagram.java
+++ assignment/src/com/CodingExercises/Question-6/Anagram.java
@@ -7,31 +7,16 @@ public class Anagram {
         Scanner scanner = new Scanner(System.in);
         String string1 = scanner.nextLine();
         string1 = string1.toLowerCase();
+        
         String string2 = scanner.nextLine();
         string2 = string2.toLowerCase();
 
-        //check if length is same
-        if (string1.length() == string2.length()) {
-
-            // convert strings to char array
-            char[] charArray1 = string1.toCharArray();
-            char[] charArray2 = string2.toCharArray();
-
-            // sort the char array
-            Arrays.sort(charArray1);
-            Arrays.sort(charArray2);
-
-            // if sorted char arrays are same
-            // then the string is anagram
-            boolean output = Arrays.equals(charArray1, charArray2);
-
-            if (output) {
-                System.out.println("Anagrams.");
-            } else {
-                System.out.println("Not Anagrams.");
-            }
+        int output = Getanagram.getanagram(string1, string2);
+        if (output == 1) {
+            System.out.println("Anagrams.");
+        } else {
+            System.out.println("Not Anagrams.");
         }
     }
 }
-
 
