3362c9a0e24e13708f40a0f120967ff761a8998d
Naresh <naresh9badisagmail.com>
2021-07-22 12:57:25 +0530

    Naresh coding Exercises

---
+++ assignment/src/com/codingExercises/question12/Main.java
@@ -0,0 +1,14 @@
+package question12;
+
+import java.util.Scanner;
+
+//main class
+public class Main {
+
+    //main method
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String sentence = scanner.nextLine();
+        System.out.println(UserMainCode.getWordWithMaximumVowels(sentence));
+    }
+}
