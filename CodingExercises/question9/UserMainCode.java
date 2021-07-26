1a9de778ec6bd02bf83aaf0d0173c391e13df1be
Spandana <890534@cognizant.com>
2021-07-26 13:22:42 +0530

    Spandana Handson 6-10

---
+++ CodingExercises/question9/UserMainCode.java
@@ -0,0 +1,27 @@
+package question9;
+
+//Logic implemented class
+public class UserMainCode {
+
+    //returns the word with maximum vowels
+    public static String storeMaxVowelWord(String sentence) {
+        String[] words = sentence.split(" ");
+        int count = 0, max = 0;
+        String maxVowelWord = null;
+        for (String word : words) {
+            count = 0;
+            //System.out.println(word);
+            char[] characters = word.toLowerCase().toCharArray();
+            for (char character : characters) {
+                if (character == 'a' || character == 'e' || character == 'i' || character == 'o' || character == 'u') {
+                    count++;
+                }
+            }
+            if (count > max) {
+                max = count;
+                maxVowelWord = word;
+            }
+        }
+        return maxVowelWord;
+    }
+}
