89e2b433d88c1ace4f899cffad02a057931d552f
Naresh <naresh9badisagmail.com>
2021-07-27 21:06:11 +0530

    Naresh coding exercises 16 to 26 questions

---
+++ assignment/src/com/codingExercises/question21/Main.java
@@ -0,0 +1,12 @@
+package question21;
+
+import java.util.Scanner;
+
+//main class
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println(UserMainCode.getDay(scanner.next()));
+    }
+}
---
+++ assignment/src/com/codingExercises/question25/Main.java
@@ -0,0 +1,13 @@
+package question25;
+
+import java.util.Scanner;
+
+//main class
+public class Main {
+
+    //main method
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println(UserMainCode.getAge("01-01-2015",scanner.next()));
+    }
+}
