23c389fb1c9638a6656a6e53d98aba2465cc6020
Naresh <naresh9badisagmail.com>
2021-07-27 12:17:44 +0530

    Modified code

--- assignment/src/com/codingExercises/question13/Main.java
+++ assignment/src/com/codingExercises/question13/Main.java
@@ -8,6 +8,10 @@ public class Main {
     //main method
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
+	String sentence = scanner.nextLine();
+        if (sentence.length() > 100) {
+            sentence = sentence.substring(0, 100);
+	}
         System.out.println(UserMainCode.swapPairs(scanner.nextLine()));
     }
 }
