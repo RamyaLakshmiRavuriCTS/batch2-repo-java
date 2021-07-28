d9acf43d9d55a962ec1970b885bdf41c97107440
Rayudu <890828@cognizant.com>
2021-07-28 11:33:03 +0530

    Modified changes

--- assignment/src/com/Question6/Main.java
+++ assignment/src/com/Question6/Main.java
@@ -4,11 +4,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        String sentence1=scanner.nextLine();
-        String sentence2=scanner.nextLine();
-        int result=UserMainCode.getAnagram(sentence1,sentence2);
-        if(result==1){
+        Scanner scanner = new Scanner(System.in);
+        String sentence1 = scanner.nextLine();
+        String sentence2 = scanner.nextLine();
+        int result = UserMainCode.getAnagram(sentence1, sentence2);
+        if(result == 1){
             System.out.println("Anagrams");
         }else{
             System.out.println("Not Anagrams");
