1f17cf8d70162f320771c76f1969dc880ca439f1
C <890773@cognizant.com>
2021-08-04 11:00:24 +0530

    Divya coding exercises 26-35

---
+++ Codingexercises/Codingexercise26/Product.java
@@ -0,0 +1,51 @@
+package Constructoroverloading;
+
+public class Product {
+
+    private long id;
+    private String productName;
+    private String supplierName;
+
+    public Product() {
+
+    }
+
+    public Product(long id, String productName) {
+        this.id = id;
+        this.productName = productName;
+    }
+
+    public Product(long id, String productName, String supplierName) {
+        this.id = id;
+        this.productName = productName;
+        this.supplierName = supplierName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getSupplierName() {
+        return supplierName;
+    }
+
+    public void setSupplierName(String supplierName) {
+        this.supplierName = supplierName;
+    }
+
+    public void display(){
+        System.out.println("Product Id is "+this.id+"\nProduct Name is "+this.productName+"\nSupplier Name is "+this.supplierName);
+    }
+}
---
+++ Codingexercises/Codingexercise30/Main.java
@@ -0,0 +1,32 @@
+package Setofboxes;
+
+import java.text.DecimalFormat;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Set<Box> boxSet = new HashSet<>();
+        DecimalFormat decimalFormat=new DecimalFormat("#.##");
+        System.out.println("Enter the number of Boxes");
+        int boxes = scanner.nextInt();
+        for (int i = 0; i < boxes; i++) {
+            Box box = new Box();
+            System.out.println("Enter Box "+(i+1)+" Details");
+            System.out.println("Enter Length");
+            box.setLength(scanner.nextDouble());
+            System.out.println("Enter Width");
+            box.setWidth(scanner.nextDouble());
+            System.out.println("Enter Height");
+            box.setHeight(scanner.nextDouble());
+            boxSet.add(box);
+        }
+        System.out.println("Unique Boxes in the Set are");
+        for (Box box : boxSet) {
+            System.out.println("Length=" + box.getLength() + " Width=" + box.getWidth() + " Height=" + box.getHeight() + " Volume=" + decimalFormat.format(box.volume()));
+        }
+    }
+}
